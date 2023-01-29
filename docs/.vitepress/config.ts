969775242bb7402d0f9b6a83489f138ee4b7bb78
xueming <810505339@qq.com>
2023-01-29 15:05:36 +0800

    添加Js

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -7,16 +7,18 @@ export default defineConfig({
     socialLinks: [
       { icon: 'github', link: 'https://github.com/810505339/vite-interview' }
     ],
-    sidebar: [
-      {
-        text: 'js面试',
-        items: [
-          { text: 'var let const 的区别', link: '/js/var-let-const'},
-          { text: 'Getting Started', link: '/getting-started' },
-        ],
-        collapsible: true 
-      }
-    ],
+    sidebar:{
+      '/js/': [
+        {
+          text: 'js面试',
+          items: [
+            { text: 'var let const 的区别', link: '/js/var-let-const'},
+            { text: '防抖和节流', link: '/js/防抖和节流' },
+          ],
+          collapsible: true 
+        }
+      ]
+    },
   },
   description: 'A VitePress site'
 })
