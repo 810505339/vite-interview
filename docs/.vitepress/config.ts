f175ec33c6a4c6051ffcab02c8cdec77a5b18238
xueming <810505339@qq.com>
2023-01-31 10:46:46 +0800

    添加

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -18,6 +18,9 @@ export default defineConfig({
             { text: '深拷贝和浅拷贝', link: '/js/深拷贝和浅拷贝' },
             { text: 'promise', link: '/js/promise' },
             { text: '渲染10w条数据', link: '/js/渲染10w条数据' },
+            { text: '闭包', link: '/js/闭包' },
+            { text: '预编译', link: '/js/预编译' },
+            { text: 'this指向面试题总结', link: '/js/this指向面试题总结' },
           ],
           collapsible: true
         }
