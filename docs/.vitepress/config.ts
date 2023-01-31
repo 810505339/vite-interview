6ee8a92793d97adc4b8e73cc09dc53f1554ec9da
xueming <810505339@qq.com>
2023-01-31 14:43:17 +0800

    新增

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -7,25 +7,32 @@ export default defineConfig({
     socialLinks: [
       { icon: 'github', link: 'https://github.com/810505339/vite-interview' }
     ],
-    sidebar: {
-      '/js/': [
-        {
-          text: 'js面试',
-          items: [
-            { text: 'var let const 的区别', link: '/js/var-let-const' },
-            { text: '防抖和节流', link: '/js/防抖和节流' },
-            { text: '继承', link: '/js/继承' },
-            { text: '深拷贝和浅拷贝', link: '/js/深拷贝和浅拷贝' },
-            { text: 'promise', link: '/js/promise' },
-            { text: '渲染10w条数据', link: '/js/渲染10w条数据' },
-            { text: '闭包', link: '/js/闭包' },
-            { text: '预编译', link: '/js/预编译' },
-            { text: 'this指向面试题总结', link: '/js/this指向面试题总结' },
-          ],
-          collapsible: true
-        }
-      ]
-    },
+    sidebar:  [
+      {
+        text: 'js面试',
+        items: [
+          { text: 'var let const 的区别', link: '/js/var-let-const' },
+          { text: '防抖和节流', link: '/js/防抖和节流' },
+          { text: '继承', link: '/js/继承' },
+          { text: '深拷贝和浅拷贝', link: '/js/深拷贝和浅拷贝' },
+          { text: 'promise', link: '/js/promise' },
+          { text: '渲染10w条数据', link: '/js/渲染10w条数据' },
+          { text: '闭包', link: '/js/闭包' },
+          { text: '预编译', link: '/js/预编译' },
+          { text: 'this指向面试题总结', link: '/js/this指向面试题总结' },
+        ],
+        collapsible: true
+      },
+      {
+        text: 'css面试',
+        items: [
+          { text: 'BFC相关', link: '/css/BFC' },
+          { text: 'flex', link: '/css/flex' },
+        ],
+        collapsible: false
+      }
+
+    ],
   },
   description: 'A VitePress site'
 })
