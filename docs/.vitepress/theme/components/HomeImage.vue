40ff0a6eab692c83dd2dd8f8c2f12ae2b729c279
xueming <810505339@qq.com>
2023-12-19 11:42:11 +0800

    跟新图标

---
+++ docs/.vitepress/theme/components/HomeImage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div h-300px w-300px ref="lavContainer"></div>
+</template>
+
+<script setup lang="ts">
+import lottie from 'lottie-web'
+import data from './json/Animation.json'
+import { onMounted, ref } from 'vue'
+const lavContainer = ref()
+
+onMounted(() => {
+  console.log(lottie)
+  if (lavContainer.value) {
+    lottie.loadAnimation({
+      container: lavContainer.value, // the dom element that will contain the animation
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      animationData: data // the path to the animation json
+    });
+  }
+
+})
+
+
+
+</script>
